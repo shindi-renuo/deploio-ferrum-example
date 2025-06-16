fc4ef62cb6d4feaec0a46cdab1e0ca8955c89ab0
Sami Hindi <sami.hindi@renuo.ch>
2025-06-16 17:18:12 +0200

    Update

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,8 @@ class HomeController < ApplicationController
   end
 
   def generate_pdf
-    browser = Ferrum::Browser.new(headless: true)
+    chromium_path = File.expand_path("../chromium-linux/chrome", __dir__)
+    browser = Ferrum::Browser.new(browser_path: chromium_path, headless: true)
     browser.go_to("http://localhost:3000/example")
     browser.pdf(path: "example.pdf", paper_width: 1.0, paper_height: 1.0)
     browser.quit()
