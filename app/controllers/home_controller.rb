17af38eb84caa4622365e8fc3971dd8c6d4cefb1
Sami Hindi <sami.hindi@renuo.ch>
2025-06-17 13:08:16 +0200

    Save to tmp/

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,9 +6,9 @@ class HomeController < ApplicationController
     Puppeteer.launch(headless: true, args: [ "--no-sandbox" ]) do |browser|
       page = browser.new_page
       page.goto("https://merry-surf.941625c.deploio.app/example")
-      page.pdf(path: "output.pdf", print_background: true, prefer_css_page_size: true, margin: { top: "0.5in", right: "0.5in", bottom: "0.5in", left: "0.5in" })
+      page.pdf(path: "tmp/output.pdf", print_background: true, prefer_css_page_size: true, margin: { top: "0.5in", right: "0.5in", bottom: "0.5in", left: "0.5in" })
       page.close
-      send_file "output.pdf", filename: "output.pdf", type: "application/pdf"
+      send_file "tmp/output.pdf", filename: "output.pdf", type: "application/pdf"
     end
   end
 end
