4850645d9356e74fddd95cf0b2b0ebc79cb59f90
Sami Hindi <sami.hindi@renuo.ch>
2025-06-17 09:52:48 +0200

    TResting

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
   end
 
   def generate_pdf
-    browser = Ferrum::Browser.new(headless: true, timeout: 20)
+    browser = Ferrum::Browser.new(browser_path: ENV.fetch("BROWSER_PATH"), headless: true, timeout: 20, process_timeout: 20)
     link = "https://#{Rails.application.routes.default_url_options[:host]}/example"
     browser.go_to(link)
     browser.pdf(path: "example.pdf", paper_width: 1.0, paper_height: 1.0)
