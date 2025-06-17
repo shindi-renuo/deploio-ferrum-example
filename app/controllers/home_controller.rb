35cefcc428db3b8b87fc6de5f2ca76247c6ba9eb
Sami Hindi <sami.hindi@renuo.ch>
2025-06-17 09:48:36 +0200

    Fix link

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,8 @@ class HomeController < ApplicationController
 
   def generate_pdf
     browser = Ferrum::Browser.new(headless: true, timeout: 20)
-    browser.go_to("http://localhost:3000/example")
+    link = "https://#{Rails.application.routes.default_url_options[:host]}/example"
+    browser.go_to(link)
     browser.pdf(path: "example.pdf", paper_width: 1.0, paper_height: 1.0)
     browser.quit()
     send_file "example.pdf", filename: "example.pdf", type: "application/pdf"
