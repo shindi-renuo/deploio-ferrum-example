a6f162f3646e7081564ab1ee262e2c2d515cf18e
Sami Hindi <sami.hindi@renuo.ch>
2025-06-17 09:58:29 +0200

    Add fallback option

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
   end
 
   def generate_pdf
-    browser = Ferrum::Browser.new(browser_path: ENV.fetch("BROWSER_PATH"), headless: true, timeout: 20, process_timeout: 20)
+    browser = Ferrum::Browser.new(browser_path: ENV.fetch("BROWSER_PATH") || Rails.root.join("chromium-linux/chrome").to_s, headless: true, timeout: 20, process_timeout: 20)
     link = "https://arrogant-aurora.941625c.deploio.app/example"
     browser.go_to(link)
     browser.pdf(path: "example.pdf", paper_width: 1.0, paper_height: 1.0)
