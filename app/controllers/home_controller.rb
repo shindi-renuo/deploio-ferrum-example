688b76faf2c84e0d10d27f2ca3f86b5464021068
Sami Hindi <sami.hindi@renuo.ch>
2025-06-17 09:58:03 +0200

    Use fixed URL

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,7 @@ class HomeController < ApplicationController
 
   def generate_pdf
     browser = Ferrum::Browser.new(browser_path: ENV.fetch("BROWSER_PATH"), headless: true, timeout: 20, process_timeout: 20)
-    link = "https://#{Rails.application.routes.default_url_options[:host]}/example"
+    link = "https://arrogant-aurora.941625c.deploio.app/example"
     browser.go_to(link)
     browser.pdf(path: "example.pdf", paper_width: 1.0, paper_height: 1.0)
     browser.quit()
