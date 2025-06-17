11bc57f42959bd580b1f7c433f06d88fc6fbe372
Sami Hindi <sami.hindi@renuo.ch>
2025-06-17 11:34:38 +0200

    Fix

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,7 +5,7 @@ class HomeController < ApplicationController
   def generate_pdf
     Puppeteer.launch(headless: true, args: [ "--no-sandbox" ]) do |browser|
       page = browser.new_page
-      page.goto("https://arrogant-aurora.941625c.deploio.app/example")
+      page.goto("https://merry-surf.941625c.deploio.app/example")
       page.pdf(path: "output.pdf", print_background: true, prefer_css_page_size: true, margin: { top: "0.5in", right: "0.5in", bottom: "0.5in", left: "0.5in" })
       page.close
       send_file "output.pdf", filename: "output.pdf", type: "application/pdf"
